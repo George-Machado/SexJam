d27f58938396bbab4811c4726ac376f8dff704be
George Machado <gm2564@nyu.edu>
2019-04-13 14:04:35 -0400

    go on this slinky dick ride with me

---
+++ Sex Jam/Assets/ThirdPersonCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+   public Transform lookAt;
+   public Transform camTransform;
+
+   private Camera _cam;
+
+   private float _distance = 10.0f;
+   private float _currentX = 0.0f;
+   private float _sensitivityX = 4.0f;
+   private float _sensitivityY = 1.0f;
+
+   private void Start()
+   {
+      camTransform = transform;
+      _cam = Camera.main;
+
+   }
+}
