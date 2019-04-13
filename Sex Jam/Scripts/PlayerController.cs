33d984f66c0ef5314bae9a6b68a9ead80cdf700c
George Machado <gm2564@nyu.edu>
2019-04-13 15:47:02 -0400

    Added player controller and camera controller

---
+++ Sex Jam/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0, ver) * speed * Time.deltaTime;
+        transform.Translate(playerMovement,Space.Self);
+
+
+    }
+}
